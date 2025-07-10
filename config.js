792c80b60195f8898caac91d5fe2b07964cddbba
dawens8 <mydawens8@gmail.com>
2025-07-10 00:45:04 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "JESUS~CRASH~V1~CowDhZJT#PL6gW9MIytpLhmd-JaS-nuQOReLELggE2l24A97vOZw",
+SESSION_ID: process.env.SESSION_ID || "JESUS~CRASH~V1~JuMUHSgL#Z_8_f8RgnDJkasplzc0OhPykC8jfAP9m4mlibf-NUkw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -53,7 +53,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
