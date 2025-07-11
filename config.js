5341df1b530b0f9fe7184d2137c6d8b853905238
dawens8 <mydawens8@gmail.com>
2025-07-10 21:52:12 -0400

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
